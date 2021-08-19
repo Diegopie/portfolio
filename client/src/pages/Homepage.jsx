dd9788f2d54ea2e9c079184ce488e1ca2abb9a49
Diego Hernandez <ishtaracademy@outlook.com>
2021-08-19 16:10:54 -0600

    working on adjusting element height based on clientHeight

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,8 +4,15 @@ import Typed from 'react-typed'
 const Homepage = () => {
 
     const clientVh = document.documentElement.clientHeight;
+    const imgHeight = clientVh/3;
+    console.log(imgHeight);
     const target = document.querySelector('body');
-    target.style.height = clientVh+'px';
+    target.style.height = clientVh + 'px';
+    // document.querySelector('#hotPic').style.height = imgHeight;
+
+    const imgStyle = {
+        height: imgHeight
+    };
 
     return (
         <main className='container justify-content-center'>
@@ -14,17 +21,16 @@ const Homepage = () => {
                     showCursor={false}
                     strings={['Diego Hernandez']}
                     typeSpeed={40}
-                    // onComplete={(self) => self.cursor.remove()}
-               />
-            </h1> 
+                // onComplete={(self) => self.cursor.remove()}
+                />
+            </h1>
             <section className='row'>
-                <p> Fist off, I am sorry for how bad this site is 😂 ( particularly my dev friends 😭 ). It's a combination of I'm bad and this shit takes a lot of time. I've kinda given up on making it look gooder.</p>
-                <p> 
-                    The Breakout Rooms page will show you the structure the Zoom meeting and the Games page doesn't do anything yet! Again, this shit takes a lot of time. The idea is it will sort us into groups and have links to the games that have them. We'll see if I get there by Saturday 😅
-                </p>
-                <p>
-                    For now, the Guest page works (ugly, but it works) and you can login to update your info and RSVP
-                </p>
+                <img
+                    id='hotPic'
+                    style={imgStyle}
+                    src='https://diegopie.herokuapp.com/assets/img/main-image-02-2.png'
+                    alt='me xD'
+                />
             </section>
         </main>
     );
