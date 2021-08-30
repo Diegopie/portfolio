6b586a81fdef4873f3a449d2a375585745157dc3
Diego Hernandez <ishtaracademy@outlook.com>
2021-08-29 19:00:47 -0600

    Removing loading on App mount using global context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 // Import Dependencies
-import React, { useCallback, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ToastContainer, Zoom } from 'react-toastify';
 // Import Pages
@@ -14,25 +14,12 @@ import './App.css';
 
 function App() {
 
-    const [{ getGuestData }, dispatch] = useGlobalContext();
-   
-    // * Fetch and Store All Guests in State
-    const runGetGuestData = useCallback(() => {
-        getGuestData()
-            .then(data => {
-                if (data) {
-                    // console.log(data);
-                    localStorage.setItem('human', JSON.stringify(data.human));
-                    dispatch({ type: 'setHuman', payload: data.human });
-                    dispatch({ type: 'setAllHuman', payload: data.allHuman });
-                    dispatch({ type: 'setAllHumanFiltered', payload: data.allHumansFiltered });
-                }
-            })
-    }, [dispatch, getGuestData])
-    
+    const [{hidesLoader}, ] = useGlobalContext();
+
+
     useEffect(() => {
-        runGetGuestData();
-    }, [runGetGuestData])
+        hidesLoader();
+    }, [hidesLoader])
 
 
     return (
