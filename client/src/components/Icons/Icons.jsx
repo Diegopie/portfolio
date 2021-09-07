e587bcb3b3e9dcc6da8fbb7d2bfa40e8c6424dac
Diego Hernandez <ishtaracademy@outlook.com>
2021-09-07 14:14:52 -0600

    organized the skills files and rectored the componante; working on styling the Modal as I add dynamic elements

--- client/src/components/Icons/Icons.jsx
+++ client/src/components/Icons/Icons.jsx
@@ -3,74 +3,73 @@ import './Icons.css';
 
 const Icons = (props) => {
 
+    const skillsArr = [
+        {
+            name: 'html',
+            src: '/img/icons/skills/html.svg',
+            alt: 'HTML 5 Icon',
+        },
+        {
+            name: 'css',
+            src: '/img/icons/skills/css.svg',
+            alt: 'CSS Icon',
+        },
+        {
+            name: 'javascript',
+            src: '/img/icons/skills/javascript.svg',
+            alt: 'JavaScript Icon',
+        },
+        {
+            name: 'jquery',
+            src: '/img/icons/skills/jquery.svg',
+            alt: 'jQuery Icon',
+        },
+        {
+            name: 'react',
+            src: '/img/icons/skills/react.svg',
+            alt: 'React Icon',
+        },
+        {
+            name: 'node',
+            src: '/img/icons/skills/node.svg',
+            alt: 'NodeJS Icon',
+        },
+        {
+            name: 'express',
+            src: '/img/icons/skills/express.svg',
+            alt: 'Express Icon',
+        },
+        {
+            name: 'handlebars',
+            src: '/img/icons/skills/handlebars.svg',
+            alt: 'Handlebars Icon',
+        },
+        {
+            name: 'mongodb',
+            src: '/img/icons/skills/mongodb.svg',
+            alt: 'MongoDB Icon',
+        },
+        {
+            name: 'mysql',
+            src: '/img/icons/skills/mysql.svg',
+            alt: 'MySQL Icon',
+        },
+        {
+            name: 'git',
+            src: '/img/icons/skills/git.svg',
+            alt: 'Git Icon',
+        }
+    ]
+
     return (
         <section className='Icons'>
-            {/* HTML */}
-            <img
-                className='Icons-icon'
-                src='/img/icons/html.svg'
-                alt='HTML 5 icon'
-            />
-            {/* CSS */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/css.svg"
-                alt='CSS 3 icon'
-            />
-            {/* JS */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/javascript.svg"
-                alt='JS icon'
-            />
-            {/* jQuery */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/jquery.svg"
-                alt='jQuery icon'
-            />
-            {/* React */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/react.svg"
-                alt='React icon'
-            />
-            {/* Node */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/node.svg"
-                alt='Node icon'
-            />
-            {/* Express */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/express.svg"
-                alt='Express icon'
-            />
-            {/* handlebars */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/handlebars.svg"
-                alt='handlebars icon'
-            />
-            {/* mongodb */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/mongodb.svg"
-                alt='mongodb icon'
-            />
-            {/* mysql */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/mysql.svg"
-                alt='mysql icon'
-            />
-            {/* git */}
-            <img
-                className="Icons-icon"
-                src="/img/icons/git.svg"
-                alt='git icon'
-            />
+            {skillsArr.map(skillData => (
+                <img
+                    className="Icons-icon"
+                    src={skillData.src}
+                    alt={skillData.alt}
+                />
+            ))}
         </section>
     );
 };
--- client/src/modals/ProjectModal/ProjectModal.jsx
+++ client/src/modals/ProjectModal/ProjectModal.jsx
@@ -29,8 +29,8 @@ const ProjectModal = (props) => {
                     alt={`${name} demo`}
                     className='ProjModal-img'
                 ></img>
-                <h3>Project Title</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi consectetur pretium nisl, eu viverra tellus. Etiam vestibulum finibus erat, quis finibus enim ultrices id. Cras rhoncus ullamcorper venenatis. Fusce sed ex ornare, ullamcorper ex sodales, semper ex. Nam vitae odio facilisis, mattis ipsum in, faucibus purus. Phasellus sapien felis, pulvinar ut massa sed, placerat aliquam lectus. Donec nec ante vestibulum, fermentum ipsum in, volutpat risus. Donec consectetur ornare elit, vel suscipit arcu suscipit vel. Nullam id nunc ut eros scelerisque imperdiet. Duis condimentum arcu ac viverra laoreet. Phasellus tellus massa, dapibus non luctus vitae, cursus eu odio. Morbi vitae nisi fringilla, viverra diam nec, condimentum purus. Suspendisse potenti. Phasellus tristique purus in dignissi</p>
+                <h3>{name}</h3>
+                <p>{projDesc}</p>
                 <article>
                     {/* GitHub */}
                     <a href="https://github.com/Diegopie/The-Smartest-Among-Us" title="View on GitHub" target="_blank" rel="noreferrer">
