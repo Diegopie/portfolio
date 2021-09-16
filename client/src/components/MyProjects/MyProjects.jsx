350fc57960253a58a62c154fe169739c3a4422b1
Diego Hernandez <ishtaracademy@outlook.com>
2021-09-16 14:42:21 -0600

    updated favivons; updated pseudo; centered modal text

--- client/src/components/MyProjects/MyProjects.jsx
+++ client/src/components/MyProjects/MyProjects.jsx
@@ -12,11 +12,11 @@ const MyProjects = (props) => {
                     stillImage={'/img/projects/among-still.png'}
                     animateImage={'/img/projects/among-animate.gif'}
                     name={'The Smartest Among Us'}
+                    projDesc={'Entertainment site for playing randomly generated quizzes. Users create their own quizzes to share with friends and compete in leaderboards'}
+                    role={'Role: Team Lead: Assigned tasks, set deadlines. UX/UI Designer and front end developer'}
                     techStack={'HTML, CSS, Bootstrap, jQuery, Open Trivia API, Node/Express Server, Sequelize, Passport, Bcrypt, Photoshop, InDesign'}
                     liveLink={'https://github.com/Diegopie/The-Smartest-Among-Us-Refactor'}
                     githubLink={'https://the-smartest-among-us.herokuapp.com/'}
-                    projDesc={'Entertainment site for playing randomly generated quizzes. Users create their own quizzes to share with friends and compete in leaderboards'}
-                    role={'Role: Team Lead: Assigned tasks, set deadlines. UX/UI Designer and front end developer'}
                 />
             </section>
             <section className='MyProjects-all'>
--- pseudo.md
+++ pseudo.md
@@ -1,11 +1,9 @@
 # Portfolio
 
-## Intro
+## Navbar
 
-1) Dark Mode Toggler Needs Styling (sun and moon img and such)
-2) Toggler Needs to Persist Upon Refresh and Re-render
-3) Toggler Needs to sync to system level dark mode
-
+## Project Modal
+Manually Pass Background color for each project?
 
 ## Languages/Skills
-Find XD, PhotoShop, Lightroom, inDesign, Illustrator, and Premier Pro SVG
+Find XD, PhotoShop, Lightroom, inDesign, Illustrator, and Premier Pro SVGk
