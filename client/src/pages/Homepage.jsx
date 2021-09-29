3c150282fe34a5c5233795b56d811046d7fbb15a
Diego Hernandez <ishtaracademy@outlook.com>
2021-09-29 16:27:49 -0600

    background color will change on darkMode

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import DummyText from '../components/helper/DummyText';
 import Icons from '../components/Icons';
 import Intro from '../components/Intro/Intro';
 import MyProjects from '../components/MyProjects/MyProjects';
 import ProjectModal from '../modals/ProjectModal/ProjectModal';
 import Transition from '../components/Transition';
+import { useGlobalContext } from '../utils/GlobalContext';
 import './css/Pages.css'
 
 const Homepage = () => {
+
+    const [{ darkMode },] = useGlobalContext();
+
+    useEffect(() => {
+        if (darkMode) {
+            document.body.classList.add('darkMode');
+            return
+        }
+        document.body.classList.remove('darkMode');
+    }, [darkMode])
 
     return (
         <main className='max-Height'>
             <Intro />
             <Icons />
-            <Transition 
+            <Transition
                 text='My Work'
                 imgLight='Transition-backImg-2-light'
                 imgDark='Transition-backImg-2-dark'
             />
             <MyProjects />
-            <Transition 
+            <Transition
                 text='About Me'
                 imgLight='Transition-backImg-3-light'
                 imgDark='Transition-backImg-3-dark'
