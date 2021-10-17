71ad750f3bfb843d0ce11ee7579b3311a7fbb3db
Diego Hernandez <ishtaracademy@outlook.com>
2021-10-16 20:52:11 -0600

    added dark mode styling to icons

--- client/src/components/helper/ContactCards/ContactCards.jsx
+++ client/src/components/helper/ContactCards/ContactCards.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useGlobalContext } from '../../../utils/GlobalContext';
 import './ContactCards.css';
 
 const ContactCards = (props) => {
+
+    const [{ darkMode }, ] = useGlobalContext();
+
+    const [darkStyle, setDarkStyle] = useState(() => {
+        return (darkMode ? 'ContactCards-darkImg' : 'null')
+    })
+
+    // Use State Change to Update DOM
+        useEffect(() => {
+        if (darkMode) {
+            setDarkStyle('ContactCards-darkImg')
+        } else {
+            setDarkStyle('null')
+        }
+    }, [darkMode])
 
     return (
         <article className='ContactCards-container'>
@@ -12,7 +28,7 @@ const ContactCards = (props) => {
                 title={props.title}
             >
                 
-                <img className='ContactCards-img'
+                <img className={`ContactCards-img ${darkStyle}`}
                     src={props.img}
                     alt={props.altText}
                 />
