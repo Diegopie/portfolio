57ae6b5d07cea60381351aebfbbeb423652603b6
Diego Hernandez <ishtaracademy@outlook.com>
2021-10-20 15:14:53 -0600

    removed unused useeffect from Homepage; fixed floating issue with contact cards

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import About from '../components/About/About';
 import Contact from '../components/Contact/Contact';
 // import DummyText from '../components/helper/DummyText';
