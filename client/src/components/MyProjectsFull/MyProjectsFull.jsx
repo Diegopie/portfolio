a0b4c991748fc966037e986ddc87fdd64f9b5d54
Diego Hernandez <ishtaracademy@outlook.com>
2021-11-01 16:23:38 -0600

    moved all project info into a js file in the data/production folder; MyProjectsAll is now rendering from an array, this may help with filtering/reordering

--- client/src/components/MyProjectsFull/MyProjectsFull.jsx
+++ client/src/components/MyProjectsFull/MyProjectsFull.jsx
@@ -1,46 +1,27 @@
 import React from 'react';
 import ProjCards from '../helper/ProjCards';
+import allProjects from '../../data/production/AllProjects';
 import './MyProjectsFull.css';
 
 const MyProjectsFull = (props) => {
+
 
     return (
         <section className=''>
             <h3 className='MyProjectsFull-header App-dropShadow '>Click to Learn More</h3>
             <section className='MyProjectsFull-container'>
-                {/* PWA Budget Tracker */}
-                <ProjCards
-                    stillImage={'/assets/img/projects/budgetPWA-still.png'}
-                    animateImage={'/assets/img/projects/budgetPWA-animate.gif'}
-                    name={'PWA Budget Tracker'}
-                    projDesc={'This demo application was made to practice PWA implementation. It is an installable app with full offline support'}
-                    role={'Role: Implement Mongo and IndexDB database so they may sync together'}
-                    techStack={'HTML, CSS, Bootstrap, Service Worker, PWA, IndexedDB, Node/Express Server, Mongoose'}
-                    githubLink={'https://github.com/Diegopie/PWA-Budget-App'}
-                    liveLink={'https://budget-pwaa.herokuapp.com/'}
-                />
-                {/* The Smartest Among Us */}
-                <ProjCards
-                    stillImage={'/assets/img/projects/among-still.png'}
-                    animateImage={'/assets/img/projects/among-animate.gif'}
-                    name={'The Smartest Among Us'}
-                    projDesc={'Entertainment site for playing randomly generated quizzes. Users can save and edit randomly generated quizzes or create their own. Any saved quiz is shareable with friends to compete in leaderboards'}
-                    role={'Role: Role: Team Lead - Assign tasks, set deadlines. Architect database, UX/UI Lead, front-end dev'}
-                    techStack={'HTML, CSS, Bootstrap, jQuery, Open Trivia API, Node/Express Server, Sequelize, Passport, Bcrypt, Photoshop, InDesign'}
-                    githubLink={'https://github.com/Diegopie/The-Smartest-Among-Us-Refactor'}
-                    liveLink={'https://the-smartest-among-us.herokuapp.com/'}
-                />
-                {/* Water Cooler */}
-                <ProjCards
-                    stillImage={'/assets/img/projects/waterCooler-still.png'}
-                    animateImage={'/assets/img/projects/waterCooler-animate.gif'}
-                    name={'Water Cooler'}
-                    projDesc={'This video conferencing application uses a unique GUI interface for interacting with large social groups. Users can use sprites to create new social spaces with unique video and text chats'}
-                    role={'Role: Full-Stack dev. Wrote friend request, room invitations, social space request systems, and connected front end UI with live database info. Managed architecting database, managing seed data, and UX/UI design'}
-                    techStack={'React, CSS, Bootstrap, React-Toastify, Service Workers, PWA, Node/Express Server, Mongoose, Jest, socket.io, Passport, Bcrypt, JWT, Eslint, Illustrator, Photoshop, XD'}
-                    githubLink={'https://github.com/Diegopie/water-cooler-refactor'}
-                    liveLink={'https://water-cooler-app.herokuapp.com/'}
-                />
+                {allProjects.map(projData => (
+                    <ProjCards
+                    name={projData.name}
+                    projDesc={projData.projDesc}
+                    role={projData.role}
+                    techStack={projData.techStack}
+                    githubLink={projData.githubLink}
+                    liveLink={projData.liveLink}
+                    stillImage={projData.stillImage}
+                    animateImage={projData.animateImage}
+                    />
+                ))}
             </section>
         </section>
     );
