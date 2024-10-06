26b9857f65d43805318b0962fbfab47ac7317400
Diego Hernandez <diego@udundi.com>
2024-10-06 17:58:58 -0600

    fixed home import, actually added photos

---
+++ client/src/components/MyExperience/MyExperience.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';
+import ProjCards from '../helper/ProjCards';
+import { useGlobalContext } from '../../utils/GlobalContext';
+import './MyExperience.css';
+
+const MyExperience = (props) => {
+
+    const [{ darkMode },] = useGlobalContext();
+
+    // - I'm thinking we can change the Projects button to an onClick listener that saves userYScroll in a variable. Then maybe run Link or Redirect to send that info to /projects, so when the return home we can maintain their scroll position;
+    // const userYScroll = window.scrollY;
+    // console.log(userYScroll);
+
+    // const buttonRef = useRef(null)
+
+    // useEffect(() => {
+    //     darkMode ? buttonRef.current.classList.add('App-Button-Main-Dark') : buttonRef.current.classList.remove('App-Button-Main-Dark');
+    // }, [darkMode]);
+
+
+    return (
+        <section>
+            <h3 className='MyProjects-header'>Click to Learn More</h3>
+            <section className='MyProjects-container'>
+                {/* PWA Budget Tracker */}
+                <ProjCards
+                    name='Udundi'
+                    projDesc={`Udundi is a custom web design and development agency that specializes in creating tailored digital solutions for businesses. 
+                    We offer services in website design, development, digital marketing, A/B Testing and eCommerce solutions, and App development. Udundi focuses on delivering high-quality, user-friendly, and responsive web experiences. 
+                    I have contributed to a variety of projects including <a href="https://www.udundi.com/case-studies/cozy-earth">Cozy Earth</a> and <a href="https://www.udundi.com/case-studies/blomberg">Blomberg</a>
+`}
+                    role='Developer and QA Engineer'
+                    techStack='Liquid, Vue, Alpine, Alpine AJAX, Glider, Bootstrap, Tailwind, Web Components, TypeScript, Sveltekit'
+                    githubLink={null}
+                    liveLink='https://www.udundi.com/'
+                    linkedInLink='https://www.linkedin.com/company/udundi/'
+                    stillImage='/assets/img/experience/udundi-still.png'
+                    animateImage={null}
+                />
+                {/* Compose */}
+                <ProjCards
+                    stillImage={'/assets/img/experience/compose-still.png'}
+                    animateImage={null}
+                    name={'Compose'}
+                    projDesc={'Compose offers affordable A/B testing for eCommerce businesses, allowing them to optimize websites using data-driven experimentation. We supports features like A/B and split URL tests, audience segmentation, custom goals, and Shopify integration, all within a pay-as-you-go pricing model. Compose aims to make conversion rate optimization accessible with no-code solutions and real-time analytics, enabling businesses to grow efficiently.'}
+                    role={'QA Engineer'}
+                    linkedInLink={'https://www.linkedin.com/company/composeco/posts/'}
+                    liveLink={'https://www.compose.co/'}
+                />
+                {/* Devmountain */}
+                <ProjCards
+                    stillImage={'/assets/img/experience/devmountain-still.png'}
+                    animateImage={null}
+                    name={'Devmountain'}
+                    projDesc={`I Led a classroom of ambitious students seeking to enhance their skills and career prospects. As an instructor, I guided learning through structured lessons, hands-on projects, and real-world applications. I collaborated with a team of Teacher Assistants to ensure personalized attention and foster a supportive learning environment. 
+                    Together, we maximized each student's learning potential by offering targeted feedback and adapting teaching methods to meet individual needs. This experience has brought me so much joy and I honer the responsibility of helping students achieve their educational and professional goals.`}
+                    role={'Instructor'}
+                    techStack={'HTML, CSS, JavaScript, NodeJS, Nunkucks, Express, Postgres, Redux, Git, Github, Sequelize, PWA, TypeScript, jQuery, OOP, DataStructures, Jest, Vite, React, SQL'}
+                    linkedInLink={'https://www.linkedin.com/school/devmountain/posts/'}
+                    liveLink={'https://devmountain.com/'}
+                />
+            </section>
+            {/* <section className='MyProjects-all'>
+                <Link to="/projects">
+                    <button className='App-Button-Main' ref={buttonRef}>View More Projects</button>
+                </Link>
+            </section> */}
+        </section>
+    );
+};
+
+export default MyExperience;
